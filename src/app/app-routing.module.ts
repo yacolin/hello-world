1d806e954ad413106db16ef89396924a01ac3f41
yacolin <mcolinqi@sina.com>
2021-01-15 18:59:32 +0800

    feat: lifecycle communication模块

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,20 @@
  * @Author: xjq
  * @Date: 2021-01-05 19:10:46
  * @LastEditors: xjq
- * @LastEditTime: 2021-01-10 17:10:54
+ * @LastEditTime: 2021-01-15 17:23:59
  * @Description: 路由
- * @FilePath: \hello-world\src\app\app-routing.module.ts
+ * @FilePath: /hello-world/src/app/app-routing.module.ts
  */
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { PeekABooParentComponent } from './lifecycle/peek-a-boo-parent/peek-a-boo-parent.component';
-import { SpyComponent } from './lifecycle/spy/spy.component';
-import { OnChangesComponent } from './lifecycle/on-changes/on-changes.component';
+import { LifecycleComponent } from './lifecycle/lifecycle.component';
+import { CommunicationComponent } from './communication/communication.component';
 
 const routes: Routes = [
-  { path: 'peek-a-boo-parent', component: PeekABooParentComponent },
-  { path: 'spy-parent', component: SpyComponent },
-  { path: 'on-changes-parent', component: OnChangesComponent },
+  { path: 'lifecycle', component: LifecycleComponent },
+  { path: 'communication', component: CommunicationComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@
  * @Author: xjq
  * @Date: 2021-01-05 19:10:46
  * @LastEditors: xjq
- * @LastEditTime: 2021-01-10 16:14:10
+ * @LastEditTime: 2021-01-15 17:23:24
  * @Description: app module
- * @FilePath: \hello-world\src\app\app.module.ts
+ * @FilePath: /hello-world/src/app/app.module.ts
  */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -15,17 +15,22 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { LifecycleModule } from './lifecycle/lifecycle.module';
+import { CommunicationModule } from './communication/communication.module';
 
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, PageNotFoundComponent],
+  declarations: [
+  AppComponent,
+  PageNotFoundComponent
+],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     CommonModule,
     LifecycleModule,
+    CommunicationModule
   ],
   providers: [],
   bootstrap: [AppComponent],
