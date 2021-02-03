70d04dd4cea10b1ba2298b9b1f4c9af9bc532412
yacolin <mcolinqi@sina.com>
2021-02-03 22:17:56 +0800

    feat: ng-zorro-antd尝试

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,40 @@
  * @Author: xjq
  * @Date: 2021-01-05 19:10:46
  * @LastEditors: xjq
- * @LastEditTime: 2021-01-10 16:40:48
+ * @LastEditTime: 2021-01-30 13:36:59
  * @Description: app component
  * @FilePath: \hello-world\src\app\app.component.ts
  */
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hello-world';
+
+  date = new Date();
+  dateArr: string[] = [];
+  disabledDate: any;
+
+  ngOnInit() {
+    this.getDateArr();
+  }
+
+  getDateArr() {
+    setTimeout(() => {
+      const _dateArr = moment(new Date()).format('YYYY-MM-DD');
+      this.dateArr = [_dateArr];
+      this.disabledDate = (current: Date): boolean => {
+        const flag = this.dateArr.includes(
+          moment(current).format('YYYY-MM-DD')
+        );
+        console.log(current, flag);
+        return !flag;
+      };
+    }, 3000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,37 +2,44 @@
  * @Author: xjq
  * @Date: 2021-01-05 19:10:46
  * @LastEditors: xjq
- * @LastEditTime: 2021-01-15 17:23:24
+ * @LastEditTime: 2021-01-30 13:18:03
  * @Description: app module
- * @FilePath: /hello-world/src/app/app.module.ts
+ * @FilePath: \hello-world\src\app\app.module.ts
  */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { LifecycleModule } from './lifecycle/lifecycle.module';
 import { CommunicationModule } from './communication/communication.module';
 
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import zh from '@angular/common/locales/zh';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+registerLocaleData(zh);
 
 @NgModule({
-  declarations: [
-  AppComponent,
-  PageNotFoundComponent
-],
+  declarations: [AppComponent, PageNotFoundComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     CommonModule,
     LifecycleModule,
-    CommunicationModule
+    CommunicationModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzDatePickerModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
